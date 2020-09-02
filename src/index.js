402a128a972cb65b26e47f23020480a7f431dbdc
Felipe Genuino <felipe.genuino@softplan.com.br>
2020-09-01 22:50:28 -0300

    Base

--- src/index.js
+++ src/index.js
@@ -1,8 +1,39 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, StatusBar } from 'react-native'
+
+/* Não possuem valor semântico
+ * Não possuem estilizacao propria
+ * Todos components possuem por padrao display:flex
+ *  
+ * View: div, footer, header, main, aside, section
+ * Text: p, span, strong, h1, h2, h3
+ */
+
 
 export default function App() {
-  return <View >
-    <Text> Oie </Text>
-  </View>
+  return (
+    <>
+      <StatusBar
+        barStyle="light-content"
+        hidden
+      />
+      <View style={styles.container}>
+        <Text style={styles.title}> Hello Evento </Text>
+      </View>
+    </>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#8de9e5',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  title: {
+    color: '#fff',
+    fontSize: 32,
+    fontWeight: 'bold'
+  }
+})
